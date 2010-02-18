79fb47dfe102ee822cbbcfab4f554b4bb8649a38
kumagai <kumagai@pluto.(none)>
2010-02-18 16:48:53 +0900

    - Copied bin/rbgrep.rb from the Cygwin.

---
+++ bin/rbgrep.rb
@@ -0,0 +1,153 @@
+#! /usr/bin/env ruby
+
+require 'optparse'
+
+
+class RubyGrep
+  MAX_LINES_FOR_MULTI_LINES = 20
+  JOINT_FOR_MULTI_LINES = ' '
+
+  def initialize(argv)
+    compile_option = 0
+    prepare_options(argv)
+    # compile_option = ?
+
+    pattern = argv.shift
+    @re = Regexp.compile(pattern, compile_option)
+
+    @filenames = argv
+    check_file_existence
+  end
+
+  def grep
+    do_grep
+  end
+
+  private
+
+    def do_grep
+      multi_lines = Array.new
+
+      if dir = @options[:r]
+        dir.chomp!('/')
+        glob_filenames = Array.new
+        @filenames.each do |filename|
+          glob_filenames += Dir.glob("#{dir}/**/#{filename}")
+        end
+        @filenames = glob_filenames
+      end
+
+      @filenames.each do |filename|
+        count = 0
+        File.open(filename, 'r').each do |line|
+          matched, is_over_multi_lines = match(line, multi_lines)
+
+          if matched
+            count += 1
+            multi_lines = Array.new
+            finishes_file = print_matched(matched, filename, is_over_multi_lines)
+            break if finishes_file
+          end
+        end
+
+        if @options[:c] && count > 0
+          puts "#{filename}: #{count}"
+        end
+      end
+    end
+
+    def match(line, multi_lines)
+      line.chomp!
+      matched = nil
+      is_over_multi_lines = false
+      if @re =~ line
+        matched = line
+      elsif @options[:m]
+        multi_lines << line
+        max_lines = MAX_LINES_FOR_MULTI_LINES
+        if max_lines > 0 && multi_lines.size > max_lines
+          multi_lines = multi_lines[-max_lines .. -1]
+        end
+        matched = multi_line_match(multi_lines)
+        is_over_multi_lines = true
+      end
+
+      return matched, is_over_multi_lines
+    end
+
+    # Last element of multi_lines (Array of String) is newly added line.
+    # Match will not achieved if trying without the new line.
+    # Neither if trying with only the new line.
+    def multi_line_match(multi_lines)
+      (multi_lines.size - 2).downto(0) do |index_start|
+        concat_line = multi_lines[index_start .. -1].join(JOINT_FOR_MULTI_LINES)
+        return multi_lines[index_start .. -1].join("\n") if @re =~ concat_line
+      end
+
+      return nil
+    end
+
+    def print_matched(matched, filename, is_over_multi_lines)
+      if @options[:l]
+        puts filename
+        return true
+      elsif ! @options[:c]
+        print "#{filename}: " if @filenames.size > 1
+        prefix, postfix = is_over_multi_lines ? ["[multi]>>\n", "\n<<[multi]"] : ["", ""]
+        puts "#{prefix}#{matched}#{postfix}"
+        return false
+      end
+    end
+
+    def prepare_options(argv)
+      @options = Hash.new { |h, k| h[k] = nil }
+      opt_parser = OptionParser.new
+      opt_parser.on("-c", "--count"                ) { |v| @options[:c] = true }
+      opt_parser.on("-l", "--files-with-matches"   ) { |v| @options[:l] = true }
+      opt_parser.on("-m", "--multi-lines"          ) { |v| @options[:m] = true }
+      opt_parser.on("-r", "--recursive DIR"        ) { |v| @options[:r] = v }
+      opt_parser.parse!(argv)
+    end
+
+    def check_file_existence
+      return if @options[:r]
+
+      @filenames.each do |filename|
+        unless File.exist?(filename)
+          STDERR.puts "Cannot find file '#{filename}'"
+          exit
+        end
+      end
+    end
+end
+
+
+if __FILE__ == $0
+  ruby_grep = RubyGrep.new(ARGV)
+  ruby_grep.grep
+end
+
+
+# The followings are currently not in use.
+
+OPTIONS = [
+  ["-c", "--count"],
+  ["-l", "--files-with-matches"],
+]
+
+class ProgramConfig
+  attr_reader :parser
+
+  def initialize
+    @hash_config = Hash.new { |h, k| h[k] = false }
+    @parser = OptionParser.new
+  end
+
+  def on(*opts, &block)
+    @parser.on(*opts, &block)
+  end
+
+  def parse!(argv)
+    @parser.parse!(argv)
+  end
+end
