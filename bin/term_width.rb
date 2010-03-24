e41e536626f5e8ea1f89f985ea315004198d2211
kumagai <kumagai@pluto.(none)>
2010-03-24 11:25:20 +0900

    [2010-03-24 11:25 @pluto] bin/term_width.rb - Newly created.

---
+++ bin/term_width.rb
@@ -0,0 +1,17 @@
+#! /bin/env ruby
+
+if ARGV.size != 1 || (n = ARGV[0].to_i) <= 0
+  $stderr.puts "#{$0}: Specify one positive integer"
+end
+
+(n / 10).times do |i|
+  s = ' ' * 9 + (i + 1).to_s
+  print s[-10 .. -1]
+end
+puts
+
+n.times do |i|
+  print((i + 1) % 10)
+end
+puts
+
