590add3e812eecb225a6374fe3a42aac354af700
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2010-03-27 09:08:12 +0900

    [2010-03-27 09:08 @macbook] unit_test/lib/test_kuma.rb - Renamed test_non_empty_string() to test_non_empty_string_to_be_false()   and added additional test targets. - Added test_non_empty_string_to_be_true().

--- unit_test/lib/test_kuma.rb
+++ unit_test/lib/test_kuma.rb
@@ -6,15 +6,31 @@ require 'kuma'
 
 class TestKumaStrUtil < Test::Unit::TestCase
 
-  def test_non_empty_string
+  def test_non_empty_string_to_be_false
     false_args = [
-      [nil], [""], [nil, ""], ["", nil], [" ", "  "]
+      [nil], [""], [nil, ""], ["", nil], [nil, " "], [" ", nil], [" ", "", nil],
+      [1], [1, " "], [" ", 1], ["a", " ", 1],
     ]
     false_args.each do |arg|
-      arg_display = arg.inspect[1 .. -2]
-      msg = "non_empty_string?(#{arg_display}) should be false"
+      msg = "non_empty_string?(#{arg_display(arg)}) should be false"
       assert(! Kuma::StrUtil.non_empty_string?(*arg), msg)
     end
   end
+
+  def test_non_empty_string_to_be_true
+    true_args = [
+      [" "], ["a"], [" ", "a"], [" ", "a", "."],
+    ]
+    true_args.each do |arg|
+      msg = "non_empty_string?(#{arg_display(arg)}) should be true"
+      assert(Kuma::StrUtil.non_empty_string?(*arg), msg)
+    end
+  end
+
+    # remove '[' and ']' at both end from arg.inspect
+    def arg_display(arg)
+      return arg.inspect[1 .. -2]
+    end
+    private :arg_display
 end
 
