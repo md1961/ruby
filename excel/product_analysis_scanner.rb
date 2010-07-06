6975760e6a9342b4178fd163d8aa2e8fcd2d2d56
U-JAPEX\naoyuki.kumagai <naoyuki.kumagai@PC09-D0062.japex.co.jp>
2010-07-06 11:03:39 +0900

    Added inner class CompletionData to class ProductAnalysisScanner
    excel/product_analysis_scanner.rb
    - Added class IllegalFormatException.
    (class ProductAnalysisScanner)
    - Added initialize() to call super and prepare @completion_data.
    - Added write().
    - Modified scan() to read a well name from the first row.
    - Added inner class CompletionData.
    lib/excel_manipulator.rb
    - Added self.blank?().

--- excel/product_analysis_scanner.rb
+++ excel/product_analysis_scanner.rb
@@ -1,8 +1,19 @@
+#! /usr/bin/ruby
 
+
+require 'kconv'
 require 'lib/excel_manipulator'
 
 
+class IllegalFormatException < Exception; end
+
 class ProductAnalysisScanner < ExcelManipulator
+
+  def initialize
+    super
+    @completion_data = nil
+
+  end
 
   def scan_all(filenames)
     begin
@@ -14,21 +25,35 @@ class ProductAnalysisScanner < ExcelManipulator
     end
   end
 
+  def write
+    puts "Well Name = " + @completion_data.well_name
+  end
+
+  TARGET_SHEETNAME = 'SK-1'
+
   MIN_ROW = 10
   MAX_ROW = 50
 
   def scan(filename)
     begin
       book = open_book(filename)
-      sheet = book.Worksheets.Item('SK-1')
+      sheet = book.Worksheets.Item(TARGET_SHEETNAME)
+
+      rows = Array.new
       i = 0
       sheet.UsedRange.Rows.each do |row|
-        records = Array.new
+        cells = Array.new
         row.Columns.each do |cell|
-          records << cell.Value
+          cells << cell.Value
         end
-        break if records.all? { |r| r.nil? or r == '' } and i >= MIN_ROW - 1
-        puts records.join(", ")
+        break if cells.all? { |r| ExcelManipulator.blank?(r) } and i >= MIN_ROW - 1
+
+        #puts cells.join(', ')
+        rows << cells
+        if @completion_data.nil? and rows.size == CompletionData::NUM_ROWS_NEEDED_TO_INITIALIZE
+          @completion_data = CompletionData.new(rows)
+        end
+
         i += 1
         break if i >= MAX_ROW
       end
@@ -36,15 +61,38 @@ class ProductAnalysisScanner < ExcelManipulator
       book.Close
     end
   end
-end
 
-class CompletionData
+  class CompletionData
+    attr_reader :well_name
+
+    NUM_ROWS_NEEDED_TO_INITIALIZE = 3
+
+    def initialize(rows)
+      read(rows)
+    end
+
+      def read(rows)
+        rows_no_blank = Array.new
+        rows.each do |row|
+          rows_no_blank << row.select { |cell| ! ExcelManipulator.blank?(cell) }
+        end
+
+        row = rows_no_blank[0]
+        @well_name = row[0]
+        unless row[1][0, 10] == '成功年月日'.tosjis
+          raise IllegalFormatException.new("No '#{"成功年月日".tosjis}' in first row")
+        end
+
+      end
+      private :read
+  end
 end
 
 
 if __FILE__ == $0
   pas = ProductAnalysisScanner.new
   pas.scan_all(%w(excel/gas_ms-29lower.xls))
+  pas.write
 end
 
 
--- lib/excel_manipulator.rb
+++ lib/excel_manipulator.rb
@@ -17,6 +17,10 @@ class ExcelManipulator
     return @excel.Workbooks.open(abs_filename)
   end
 
+  def self.blank?(value)
+    return value.nil? || value == ''
+  end
+
     def get_absolute_path(filename)
       fso = WIN32OLE.new('Scripting.FileSystemObject')
       return fso.GetAbsolutePathName(filename)
