fdd2df73bfce8f8d03010abfdfb5b532915a65f9
Naoyuki Kumagai <kumagai@uranus>
2010-11-16 13:30:28 +0900

    Added a class to include and test ImplEach

--- impl_each.rb
+++ impl_each.rb
@@ -1,7 +1,35 @@
 
 module ImplEach
-  def each(&f_proc)
-    @eachees.each { |eachee| f_proc.call(eachee) }
+  def each
+    @eachees.each do |eachee|
+      yield eachee
+    end
+  end
+end
+
+
+class TestEach
+  include ImplEach, Enumerable
+
+  def initialize(*args)
+    @eachees = args.dup
+  end
+end
+
+
+if __FILE__ == $0
+  te = TestEach.new(*((0 .. 9).to_a))
+
+  te.each_with_index do |item, index|
+    puts "item = #{item}, index = #{index}"
+  end
+
+  iter = te.each
+  begin
+    while true
+      puts "item = #{iter.next}"
+    end
+  rescue StopIteration
   end
 end
 
