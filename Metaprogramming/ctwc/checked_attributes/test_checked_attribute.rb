d9f18f157e8a3c6687ca464dd30371324f8c1258
Naoyuki Kumagai <kumagai@ariel>
2012-03-06 11:58:47 +0900

    [Metaprogramming] Solve quiz in test_checked_attribute.rb
    Metaprogramming/ctwc/checked_attributes/test_checked_attribute.rb
    - Implement add_checked_attribute to pass the unit test.

--- Metaprogramming/ctwc/checked_attributes/test_checked_attribute.rb
+++ Metaprogramming/ctwc/checked_attributes/test_checked_attribute.rb
@@ -35,6 +35,15 @@ class TestCheckedAttribute < Test::Unit::TestCase
 end
 
 def add_checked_attribute(clazz, attribute)
-  # ...
+  clazz.class_eval do
+    define_method attribute do
+      instance_variable_get(:"@#{attribute}")
+    end
+
+    define_method "#{attribute}=" do |value|
+      raise RuntimeError, 'Invalid attribute' unless value
+      instance_variable_set(:"@#{attribute}", value)
+    end
+  end
 end
 
