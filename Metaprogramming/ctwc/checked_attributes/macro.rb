b5db3278e23dc7ea8f8e38aa97b9764ceccae381
Naoyuki Kumagai <naoyuki.kumagai@gmail.com>
2012-03-07 14:28:50 +0900

    [Metaprogramming] Solve quiz in 'ctwc/checked_attributes'
    Metaprogramming/ctwc/checked_attributes/macro.rb
    - Implement open-class Class.
    Metaprogramming/ctwc/checked_attributes/module.rb
    - Implement module CheckedAttributes.

--- Metaprogramming/ctwc/checked_attributes/macro.rb
+++ Metaprogramming/ctwc/checked_attributes/macro.rb
@@ -7,6 +7,16 @@
 # Visit http://www.pragmaticprogrammer.com/titles/ppmetr for more book information.
 #---
 class Class
+  def attr_checked(attribute, &validation)
+    define_method "#{attribute}=" do |value|
+      raise 'Invalid attribute' unless validation.call(value)
+      instance_variable_set("@#{attribute}", value)
+    end
+
+    define_method attribute do
+      instance_variable_get("@#{attribute}")
+    end
+  end
 end
 
 require 'test/unit'
