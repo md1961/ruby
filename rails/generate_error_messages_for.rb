57d1720e8eef69669886f18a5f7f1dfab91d394c
Naoyuki Kumagai <kumagai@namaka>
2016-07-27 11:16:30 +0900

    [generate_error_messages_for] Correct to notify if error_messages_for() exists
    rails/generate_error_messages_for.rb
    - Correct to notify at the end of execution if error_messages_for()
      already exists in application_helper.rb.

--- rails/generate_error_messages_for.rb
+++ rails/generate_error_messages_for.rb
@@ -23,6 +23,7 @@ DIR_STYLESHEET = File.join(%w(app assets stylesheets)).freeze
 
 TARGET_HELPER        = 'application_helper.rb'
 SOURCE_HELPER_APPEND = 'application_helper_append.rb'
+HELPER_METHOD_NAME   = 'error_messages_for'
 
 SOURCE_TEMPLATE   = '_error_messages_for.html.erb'
 SOURCE_STYLESHEET = 'errors.css'
@@ -30,22 +31,22 @@ SOURCE_STYLESHEET = 'errors.css'
 
 # app/helpers/application_helper.rb
 
-RE_METHOD_IN_HELPER = /\A\s*def\s+error_messages\for/
+RE_METHOD_IN_HELPER = /\A\s*def\s+#{HELPER_METHOD_NAME}/
 RE_END_OF_HELPER_BODY = /\Aend\s*\z/
 
 f_tmp = Tempfile.open(TARGET_HELPER)
-is_method_written = false
-method_exists = false
+is_helper_method_written = false
+helper_method_exists = false
 File.open(File.join(DIR_HELPER, TARGET_HELPER), 'r') do |f|
   f.each do |line|
-    method_exists = true if line =~ RE_METHOD_IN_HELPER
-    if line =~ RE_END_OF_HELPER_BODY && ! is_method_written
+    helper_method_exists = true if line =~ RE_METHOD_IN_HELPER
+    if line =~ RE_END_OF_HELPER_BODY && ! is_helper_method_written
       File.open(File.join(DIR_SOURCE, SOURCE_HELPER_APPEND)) do |f2|
         f2.each do |line2|
           f_tmp.write line2
         end
       end
-      is_method_written = true
+      is_helper_method_written = true
     end
     f_tmp.write line
   end
@@ -64,4 +65,10 @@ FileUtils.cp(File.join(DIR_SOURCE, SOURCE_TEMPLATE), DIR_TEMPLATE)
 # app/assets/stylesheets/errors.css
 
 FileUtils.cp(File.join(DIR_SOURCE, SOURCE_STYLESHEET), DIR_STYLESHEET)
+
+
+if helper_method_exists
+  STDERR.puts "#{HELPER_METHOD_NAME}() already exists in #{TARGET_HELPER}, but append a method anyway."
+  STDERR.puts "Remove either method definition."
+end
 
