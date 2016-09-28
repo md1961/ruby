11d0c8c03d0ae9c641a6458a474314f326231dec
Naoyuki Kumagai <kumagai@namaka>
2016-09-28 15:14:44 +0900

    Create module Kuma::Color for simple esc seq coloring

---
+++ lib/kuma/color.rb
@@ -0,0 +1,13 @@
+module Kuma
+
+  module Color
+    RED     = "\e[31m"
+    GREEN   = "\e[32m"
+    YELLOW  = "\e[33m"
+    BLUE    = "\e[34m"
+    MAGENTA = "\e[35m"
+    CYAN    = "\e[36m"
+    WHITE   = "\e[37m"
+    RESET   = "\e[0m"
+  end
+end
