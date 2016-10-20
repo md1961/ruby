b13461304a2e2601c3f818df90c48896686d32e9
Naoyuki Kumagai <kumagai@namaka>
2016-10-20 11:11:20 +0900

    [files_to_commit_git] Receive options to open files with vim

--- bin/files_to_commit_git.rb
+++ bin/files_to_commit_git.rb
@@ -26,7 +26,21 @@ Changes to be committed:
         new file:   diff.all.txt
 =end
 
+opens_with_vim = false
+if ARGV[0] == '-o' || ARGV[0] == '-p'
+  opens_with_vim = true
+elsif ARGV[0]
+  STDERR.puts "Usage: #{File.basename($0)} [-[op]] (pass options to vim)"
+  exit
+end
+
 INDENTS = [' ' * 8, "\t"]
 
 line_with_file = `git status`.split("\n").select { |line| line =~ /\A#{INDENTS.join('|')}/ }
-puts line_with_file.map { |line| line.sub(/\A\s*(?:modified:|new file:)?\s*/, '') }.join("\n")
+filenames = line_with_file.map { |line| line.sub(/\A\s*(?:modified:|new file:)?\s*/, '') }
+
+if opens_with_vim
+  system("vim #{ARGV[0]} #{filenames.join(' ')}")
+else
+  puts filenames.join("\n")
+end
