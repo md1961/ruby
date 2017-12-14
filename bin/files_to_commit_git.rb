0131a05bfd8734958ee5127f6b45b47919fb8e99
Naoyuki Kumagai <kumagai@mimas>
2017-12-14 09:10:30 +0900

    [files_to_commit_git] Use filename(s) under if directory

--- bin/files_to_commit_git.rb
+++ bin/files_to_commit_git.rb
@@ -1,31 +1,5 @@
 #! /bin/env ruby
 
-=begin
-On branch problem03
-Your branch is up-to-date with 'origin/problem03'.
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git checkout -- <file>..." to discard changes in working directory)
-
-        modified:   README.md
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-
-        diff.all.txt
-
-no changes added to commit (use "git add" and/or "git commit -a")
-[kumagai@namaka asagao-extended]$ git add .
-[kumagai@namaka asagao-extended]$ git st
-On branch problem03
-Your branch is up-to-date with 'origin/problem03'.
-Changes to be committed:
-  (use "git reset HEAD <file>..." to unstage)
-
-        modified:   README.md
-        new file:   diff.all.txt
-=end
-
 opens_with_vim = false
 if ARGV[0] == '-o' || ARGV[0] == '-p'
   opens_with_vim = true
@@ -37,7 +11,12 @@ end
 INDENTS = [' ' * 8, "\t"]
 
 line_with_file = `git status`.split("\n").select { |line| line =~ /\A#{INDENTS.join('|')}/ }
-filenames = line_with_file.map { |line| line.sub(/\A\s*(?:modified:|new file:)?\s*/, '') }
+filenames = \
+  line_with_file.map { |line|
+    line.sub(/\A\s*(?:modified:|new file:)?\s*/, '')
+  }.flat_map { |filename|
+    filename.end_with?('/') ? Dir.glob("#{filename}*") : filename
+  }
 
 if opens_with_vim
   system("vim #{ARGV[0]} #{filenames.join(' ')}")
