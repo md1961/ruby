747087e54814cc562394189ae2b272d7a6941bb9
Naoyuki Kumagai <kumagai@mimas>
2018-06-12 14:53:57 +0900

    [migration_edit] Modify to receive file No. as command line arguments

--- rails/migration_edit.rb
+++ rails/migration_edit.rb
@@ -11,23 +11,25 @@ end
 
 filenames = Dir.glob('db/migrate/*.rb').sort
 
-filenames.each.with_index(1) do |filename, index|
-  migration_name = filename.sub(%r!db/migrate/\d+_!, '').gsub('_', ' ').gsub(/\.rb\z/, '')
-  migration_name.sub!(/\b(create |to )(\w.*)\z/) {
-    model_name_parts = $2.split
-    model_name_parts[-1] = model_name_parts.last.singularize
-    $1 + model_name_parts.map(&:capitalize).join
-  }
-  puts format('%3d: %s', index, migration_name)
-end
+indexes = ARGV.map(&:to_i).reject(&:zero?).map { |i| i - 1 }
+if indexes.empty?
+  filenames.each.with_index(1) do |filename, index|
+    migration_name = filename.sub(%r!db/migrate/\d+_!, '').gsub('_', ' ').gsub(/\.rb\z/, '')
+    migration_name.sub!(/\b(create |to )(\w.*)\z/) {
+      model_name_parts = $2.split
+      model_name_parts[-1] = model_name_parts.last.singularize
+      $1 + model_name_parts.map(&:capitalize).join
+    }
+    puts format('%3d: %s', index, migration_name)
+  end
 
-indexes = []
-puts '--------------------------------'
-loop do
-  str_numbers = Readline.readline("Enter No.'s to edit: ")
-  exit unless str_numbers =~ /\A[\d ]+\z/
-  indexes = str_numbers.split.map { |n| Integer(n) - 1 }
-  break if indexes.all? { |index| index < filenames.size }
+  puts '--------------------------------'
+  loop do
+    str_numbers = Readline.readline("Enter No.'s to edit: ")
+    exit unless str_numbers =~ /\A[\d ]+\z/
+    indexes = str_numbers.split.map { |n| Integer(n) - 1 }
+    break if indexes.all? { |index| index < filenames.size }
+  end
 end
 
 filenames_to_edit = filenames.values_at(*indexes)
