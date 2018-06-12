eaefdf87ca879f167d661edc2731738668c9b551
Naoyuki Kumagai <kumagai@mimas>
2018-06-12 13:43:13 +0900

    [migration_edit] Modify to camelize model names in menu

--- rails/migration_edit.rb
+++ rails/migration_edit.rb
@@ -2,6 +2,7 @@
 
 require 'readline'
 require_relative 'rails_util'
+require 'active_support/inflector'
 
 unless RailsUtil.rails_dir?
   STDERR.puts "Quit execution as you're not in a Rails root directory."
@@ -11,7 +12,13 @@ end
 filenames = Dir.glob('db/migrate/*.rb').sort
 
 filenames.each.with_index(1) do |filename, index|
-  puts format('%3d: %s', index, filename.sub(%r!db/migrate/\d+_!, ''))
+  migration_name = filename.sub(%r!db/migrate/\d+_!, '').gsub('_', ' ').gsub(/\.rb\z/, '')
+  migration_name.sub!(/\b(create |to )(\w.*)\z/) {
+    model_name_parts = $2.split
+    model_name_parts[-1] = model_name_parts.last.singularize
+    $1 + model_name_parts.map(&:capitalize).join
+  }
+  puts format('%3d: %s', index, migration_name)
 end
 
 indexes = []
