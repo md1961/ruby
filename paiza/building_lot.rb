03c834e319cd0fb682ed0cdfb45b59ae570264a9
Naoyuki Kumagai <kumagai@{{ remote_hostname }}>
2019-08-21 08:38:07 +0900

    Start on paiza/building_lot.rb (Prob. A in first map of 'Android')

---
+++ paiza/building_lot.rb
@@ -0,0 +1,286 @@
+require 'pry'
+
+
+class Allocator
+
+  def initialize(lot)
+    @lot = lot
+    @buildings = []
+  end
+
+  def add(building)
+    @buildings << building unless oversize?(building)
+  end
+
+  def allocate
+    @buildings.sort_by! { |b| -b.score }
+    @buildings.each do |building|
+      @lot.place(building)
+    end
+  end
+
+  def to_s
+    @lot.to_s
+  end
+
+  private
+
+    def oversize?(building)
+      building.height > @lot.height || building.width > @lot.width
+    end
+end
+
+
+class Lot
+  attr_reader :height, :width
+
+  def initialize(height, width)
+    @height = height
+    @width  = width
+    @cells = Array.new(height) { [0] * width }
+  end
+
+  def place(building)
+    y0, x0, dy, dx, dir = case building.ent_direction
+                          when :north
+                            [@height - building.height, 0, -1, 1, :x]
+                          when :south
+                            [0, 0, 1, 1, :x]
+                          when :west
+                            [0, @width - building.width, 1, -1, :y]
+                          else
+                            [0, 0, 1, 1, :y]
+                          end
+    y, x = y0, x0
+    while true
+      if placeable?(building, y, x)
+        do_place(building, y, x)
+        return true if all_fronts_connected?
+        undo_place(building, y, x)
+      end
+      if dir == :y
+        y += dy
+        if y < 0 || y >= @height
+          y = y < 0 ? @height - 1 : 0
+          x += dx
+          break if out_of_bounds?(y, x)
+        end
+      else
+        x += dx
+        if x < 0 || x >= @width
+          x = x < 0 ? @width - 1 : 0
+          y += dy
+          break if out_of_bounds?(y, x)
+        end
+      end
+    end
+    false
+  end
+
+  def to_s
+    @cells.map { |row|
+      row.map { |cell|
+        cell.is_a?(Array) ? (cell.size > 1 ? '#' : '+') \
+          : cell.zero? ? '.' : cell >= 10 ? ('A'.ord + cell - 10).chr : cell
+      }.join(' ')
+    }.join("\n")
+  end
+
+  private
+
+    def out_of_bounds?(y, x)
+      y < 0 || y >= @height || x < 0 || x >= @width
+    end
+
+    def all_fronts_connected?
+      num_buildings == num_fronts_connected_to_first_front
+    end
+
+    def num_buildings
+      @cells.flat_map { |row|
+        row.map { |cell| cell.is_a?(Array) ? 0 : cell }
+      }.uniq.size - 1
+    end
+
+    def num_fronts_connected_to_first_front
+      y0, x0 = coord_of_first_front
+      return 0 unless y0
+      num_fronts_connected_at(y0, x0).tap {
+        clear_all_checked_marks
+      }
+    end
+
+    def num_fronts_connected_at(y, x)
+      return 0 if out_of_bounds?(y, x)
+      cell = @cells[y][x]
+      return 0 if cell == :CHECKED
+      return 0 if cell.is_a?(Array) && cell.include?(:CHECKED)
+      return 0 if !cell.is_a?(Array) && cell > 0
+      if cell == 0
+        @cells[y][x] = :CHECKED
+        return 0
+      end
+      num0 = cell.is_a?(Array) ? cell.size : 0
+      cell << :CHECKED if cell.is_a?(Array)
+      [[-1, 0], [1, 0], [0, -1], [0, 1]].reduce(num0) { |num, (dy, dx)|
+        num + num_fronts_connected_at(y + dy, x + dx)
+      }
+    end
+
+    def clear_all_checked_marks
+      0.upto(@height - 1) do |y|
+        0.upto(@width - 1) do |x|
+          cell = @cells[y][x]
+          if cell.is_a?(Array) && cell.include?(:CHECKED)
+            @cells[y][x].delete(:CHECKED)
+            @cells[y][x] = 0 if cell.empty?
+          elsif cell == :CHECKED
+            @cells[y][x] = 0
+          end
+        end
+      end
+    end
+
+    def coord_of_first_front
+      0.upto(@height - 1) do |y|
+        0.upto(@width - 1) do |x|
+          return [y, x] if @cells[y][x].is_a?(Array)
+        end
+      end
+      nil
+    end
+
+    def placeable?(building, y0, x0)
+      y1 = y0 + building.height - 1
+      x1 = x0 + building.width  - 1
+      return false if y1 >= @height
+      return false if x1 >= @width
+      y0.upto(y1) do |y|
+        x0.upto(x1) do |x|
+          cell = @cells[y][x]
+          return false if cell.is_a?(Array) || cell > 0
+        end
+      end
+      y_front, x_front = building.coord_front_when_placed_on(y0, x0)
+      return false if out_of_bounds?(y_front, x_front)
+      cell_front = @cells[y_front][x_front]
+      cell_front.is_a?(Array) || cell_front == 0
+    end
+
+    def do_place(building, y0, x0)
+      raise "Cannot place #{building.inspect} at (#{y0}, #{x0})" unless placeable?(building, y0, x0)
+      y1 = y0 + building.height - 1
+      x1 = x0 + building.width  - 1
+      y0.upto(y1) do |y|
+        x0.upto(x1) do |x|
+          @cells[y][x] = building.id
+        end
+      end
+      y_front, x_front = building.coord_front_when_placed_on(y0, x0)
+      cell_front = @cells[y_front][x_front]
+      if cell_front == 0
+        @cells[y_front][x_front] = [building.id]
+      else
+        cell_front << building.id
+      end
+    end
+
+    def undo_place(building, y0, x0)
+      return if @cells[y0][x0] != building.id
+      y1 = y0 + building.height - 1
+      x1 = x0 + building.width  - 1
+      y0.upto(y1) do |y|
+        x0.upto(x1) do |x|
+          @cells[y][x] = 0
+        end
+      end
+      y_front, x_front = building.coord_front_when_placed_on(y0, x0)
+      @cells[y_front][x_front].delete(building.id)
+      @cells[y_front][x_front] = 0 if @cells[y_front][x_front].empty?
+    end
+end
+
+
+class Building
+  attr_reader :id, :height, :width, :y_ent, :x_ent
+
+  def initialize(id, height, width, y_ent, x_ent)
+    @id     = id
+    @height = height
+    @width  = width
+    @y_ent  = y_ent
+    @x_ent  = x_ent
+    _dump = ent_direction
+  end
+
+  def score
+    @height * @width
+  end
+
+  def ent_direction
+    if @y_ent == 1
+      :north
+    elsif @y_ent == @height
+      :south
+    elsif @x_ent == 1
+      :west
+    elsif @x_ent == @width
+      :east
+    else
+      raise "Illegal entrance position (#{@y_ent}, #{@x_int}) for building ##{@id}"
+    end
+  end
+
+  def coord_front_when_placed_on(y0, x0)
+    case ent_direction
+    when :north
+      [y0 - 1, x0 + @x_ent - 1]
+    when :south
+      [y0 + @height, x0 + @x_ent - 1]
+    when :west
+      [y0 + @y_ent - 1, x0 - 1]
+    else
+      [y0 + @y_ent - 1, x0 + @width]
+    end
+  end
+end
+
+
+if __FILE__ == $0
+  $stdin = DATA
+
+  height, width, n_buildings = gets.split.map(&:to_i)
+  allocator = Allocator.new(Lot.new(height, width))
+
+  n_buildings.times do |i|
+    args = gets.split.map(&:to_i)
+    allocator.add(Building.new(i + 1, *args))
+  end
+
+  allocator.allocate
+
+  puts allocator
+
+  puts allocator.instance_variable_get('@lot').send(:all_fronts_connected?)
+end
+
+
+__END__
+6 7 2
+2 4 2 3
+2 5 2 4
+
+2 3 1 2
+
+3 2 2 1
+
+5 7 4
+3 5 3 3
+3 5 2 1
+3 5 2 5
+3 5 1 3
+
+5 5 2
+2 5 2 2
+2 5 1 3
+
